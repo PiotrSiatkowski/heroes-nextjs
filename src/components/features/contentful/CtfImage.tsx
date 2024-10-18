053628355d708e6cbeedbaa6bdf7e7fadea061ca
Piotr Siatkowski <siatkowski.p@gmail.com>
2024-10-18 15:30:21 +0200

    Polishing changes #9

--- src/components/features/contentful/CtfImage.tsx
+++ src/components/features/contentful/CtfImage.tsx
@@ -2,28 +2,44 @@ import NextImage, { ImageProps as NextImageProps } from 'next/image';
 import { twMerge } from 'tailwind-merge';
 
 import { ImageFieldsFragment } from '@src/lib/__generated/sdk';
+import { useEffect, useState } from 'react';
 
 interface ImageProps extends Omit<ImageFieldsFragment, '__typename'> {
   nextImageProps?: Omit<NextImageProps, 'src' | 'alt'>;
 }
 
 export const CtfImage = ({ url, width, height, title, nextImageProps }: ImageProps) => {
-  if (!url) return null;
+  const [heroHasLoaded, setHeroHasLoaded] = useState(false);
+
+  useEffect(() => {
+    setHeroHasLoaded(false);
+  }, [url]);
+
+  if (!url) {
+    return null;
+  }
 
   const blurURL = new URL(url);
   blurURL.searchParams.set('w', '10');
 
   return (
-    <NextImage
-      src={url}
-      width={width ?? undefined}
-      height={height ?? undefined}
-      alt={title || ''}
-      sizes="(max-width: 1200px) 100vw, 50vw"
-      placeholder="blur"
-      blurDataURL={blurURL.toString()}
-      {...nextImageProps}
-      className={twMerge(nextImageProps?.className, 'transition-all')}
-    />
+    <div className="bg-black">
+      <div
+        className={`transition-opacity duration-500 ${heroHasLoaded ? 'opacity-100' : 'opacity-0'}`}
+      >
+        <NextImage
+          onLoadingComplete={() => setHeroHasLoaded(true)}
+          src={url}
+          width={width ?? undefined}
+          height={height ?? undefined}
+          alt={title || ''}
+          sizes="(max-width: 1200px) 100vw, 50vw"
+          placeholder="blur"
+          blurDataURL={blurURL.toString()}
+          {...nextImageProps}
+          className={twMerge(nextImageProps?.className, 'transition-all')}
+        />
+      </div>
+    </div>
   );
 };
